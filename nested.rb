cf8c2239f4dfd69ac06ee4c76bf73d72c926972f
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-06-29 23:30:19 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,5 +1,5 @@
 
-def hopper
+def hopper 
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -14,12 +14,13 @@ def hopper
         :known_for => "Unix",
         :languages => ["C"]
       }
-    }
+    
+ 		}
+ 		programmer_hash[:grace_hopper]
 
-end
+  end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
  		{
@@ -36,8 +37,9 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
+
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,7 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -79,9 +81,14 @@ def adding_matz
       :dennis_ritchie => {
         :known_for => "Unix",
         :languages => ["C"]
-      }
+      }, 
+      :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+     }
+     
     }
-
+  programmer_hash
 end
 
 def changing_alan
@@ -103,7 +110,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:alan_kay][:known_for]= "GUI"
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +133,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+programmer_hash
 end
